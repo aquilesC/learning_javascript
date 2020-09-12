5cc9c7e3489a475eedf4d321c9ba1f2578f312f8
Aquiles Carattino <aqui.carattino@gmail.com>
2020-09-12 18:30:59 +0200

    Added filtering

--- 01_todo_app/app.js
+++ 01_todo_app/app.js
@@ -37,7 +37,6 @@ function addTodo(event) {
 }
 
 function deleteCheck(event) {
-    console.log(event.target);
     const item = event.target;
     if (item.classList[0] === "trash-btn") {
         const todo = item.parentElement;
@@ -52,7 +51,30 @@ function deleteCheck(event) {
 }
 
 function filterTodo(e) {
-    const todos = todoList.childNodes;
+    const todos = todoList.children;
     console.log(todos);
-
+    for (let todo of todos) {
+        console.log(todo);
+    }
+    for (let todo of todos) {
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case 'uncompleted':
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    }
 }
